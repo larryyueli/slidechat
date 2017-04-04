be5f14a87b49b02589d37f41e810ac49a07e0cd0
Arjun Dhiman <arjund38@gmail.com>
2017-04-03 23:37:22 -0400

    make api folder

---
+++ api/config.php
@@ -0,0 +1,5 @@
+<?php
+
+date_default_timezone_set('America/Toronto');
+// enter database credentials here
+$dbconn = pg_connect("host=mcsdb.utm.utoronto.ca dbname=csc301app0 user=csc301app0 password=3hnkflwjcielgjs,c(");
---
+++ api/index.php
@@ -0,0 +1,10 @@
+<?php
+require '../vendor/slim/slim/Slim/Slim.php';
+require_once('config.php');
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+$app->db = $dbconn;
+
+
+$app->run();
