c4865d6f63fa27ead19cc6b21915d553f2c8076c
Arjun Dhiman <arjund38@gmail.com>
2017-08-07 17:25:58 -0400

    config

--- api/config.php
+++ api/config.php
@@ -2,17 +2,8 @@
 
 date_default_timezone_set('America/Toronto');
 // enter database credentials here
-<<<<<<< HEAD
-
-try{
-$dbconn = pg_connect("host=localhost port=5432 dbname=slidechat user=arjundhiman password=123456");
-}
-catch(Exception $e){
-  echo $e->getMessage();
-=======
 try {
-    $dbconn = pg_connect("host='' port='' dbname='' user='' password=''");
+    $dbconn = pg_connect('host=localhost port=5432 dbname=slidechat user=arjundhiman password=123456');
 } catch (Exception $e) {
     echo $e->getMessage();
->>>>>>> 75eba1f602568b75077972947b0a9f25377969f1
 }
