1e845748f0856db63bae64cab37d7fd613a17c8e
Arjun Dhiman <arjund38@gmail.com>
2017-08-12 14:15:18 -0400

    unit test

---
+++ tests/unit/ApiTest.php
@@ -0,0 +1,18 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class OutputTest extends TestCase
+{
+    public function testExpectFooActualFoo()
+    {
+        $this->expectOutputString('foo');
+        print 'foo';
+    }
+
+    public function testExpectBarActualBaz()
+    {
+        $this->expectOutputString('bar');
+        print 'baz';
+    }
+}
+?>
