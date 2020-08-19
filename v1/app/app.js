93f9979149a904ab81268466a31d0e0fd26d0ebc
larryyueli <yuelizhang@gmail.com>
2020-08-19 15:30:26 -0400

    clean up v1 and updated gitignore

--- v1/app/app.js
+++
@@ -1,9 +0,0 @@
-/*
-
-    DO NOT REMOVE THE FOLLOWING LINE OF CODE, IF THIS REMOVED THEN
-    THE APPLICATION WILL THROW BACK EXPECTIONS.
-
-*/
-
-//Init of the application
-var app = angular.module("slidechat", ['ui.router', 'pdf', 'ngSanitize']);
