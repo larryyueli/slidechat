e07b35511ed9fce634c36f27c8df586aee24e10b
Samarium <28302241+Samarium150@users.noreply.github.com>
2021-01-14 15:13:49 -0500

    build: resolve build on Windows

---
+++ v2/setup.bat
@@ -0,0 +1,8 @@
+setlocal enableextensions enabledelayedexpansion
+call npm i
+cd client
+call npm i
+cd ..
+md .slidechat\files
+call npm run build
+echo You are all almost done! Check out our GitHub Wiki for how to deploy this app or start developing.
