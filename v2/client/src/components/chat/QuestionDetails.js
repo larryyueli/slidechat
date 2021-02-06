ccc88c55be9e17e4078e5b3c73680d5a20b885bc
Aash Brar <aashdeepbrar@gmail.com>
2021-02-05 19:20:35 -0500

    Changing QuestionDetails to class

--- v2/client/src/components/chat/QuestionDetails.js
+++ v2/client/src/components/chat/QuestionDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { Button, Snackbar, TextField } from '@material-ui/core';
 
@@ -25,214 +25,228 @@ const md = markdownIt({
 });
 md.use(markdownItMathJax());
 
-export default function QuestionDetails(props) {
-	const [managing, setManaging] = useState(false);
-	const [messages, setMessages] = useState({ title: 'Loading...', chats: [] });
-	const [showToast, setShowToast] = useState(false);
-	const chatRef = useRef(null);
+export default class QuestionDetails extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			managing: false,
+			messages: { title: 'Loading...', chats: [] },
+			showToast: false,
+		};
+		this.chatRef = React.createRef();
+		this.sendNewChat = this.sendNewChat.bind(this);
+	}
 
-	useEffect(() => {
-		fetchQuestionDetails();
-		props.setDrawingOverlay(false);
-		props.setDrawing(false);
-		props.setShowTempDrawingBtn(false);
-		// eslint-disable-next-line
-	}, [props.sid, props.pageNum, props.qid]);
-
-	// render math in chat details
-	useEffect(() => {
+	componentDidMount() {
+		this.fetchQuestionDetails();
 		window.MathJax.typeset();
-	});
+	}
 
-	const fetchQuestionDetails = async () => {
+	componentDidUpdate(prevProps) {
+		if (this.props.sid !== prevProps.sid || this.props.pageNum !== prevProps.pageNum ||
+			this.props.qid !== prevProps.qid) {
+			this.fetchQuestionDetails();
+			this.props.setDrawingOverlay(false);
+			this.props.setDrawing(false);
+			this.props.setShowTempDrawingBtn(false);
+		}
+		window.MathJax.typeset();
+	}
+
+	async fetchQuestionDetails() {
 		try {
 			const res = await axios.get(
-				`${serverURL}/api/chats?slideID=${props.sid}&pageNum=${props.pageNum}&qid=${props.qid}`
+				`${serverURL}/api/chats?slideID=${this.props.sid}&pageNum=${this.props.pageNum}&qid=${this.props.qid}`
 			);
-			setMessages(res.data);
-			window.history.replaceState(null, null, `${baseURL}/${props.sid}/${props.pageNum}/${props.qid}`);
-			if (res.data.drawing && props.drawable) {
-				props.setDrawingOverlay(true);
-				props.canvasComponentRef.current.setState({ readOnly: true });
-				props.canvasComponentRef.current.lines = res.data.drawing;
-				props.canvasComponentRef.current.redraw();
+			this.setState({ messages: res.data });
+			window.history.replaceState(null, null, `${baseURL}/${this.props.sid}/${this.props.pageNum}/${this.props.qid}`);
+			if (res.data.drawing && this.props.drawable) {
+				this.props.setDrawingOverlay(true);
+				this.props.canvasComponentRef.current.setState({ readOnly: true });
+				this.props.canvasComponentRef.current.lines = res.data.drawing;
+				this.props.canvasComponentRef.current.redraw();
 			}
 		} catch (err) {
 			console.error(err);
 		}
-	};
+	}
 
-	const toggleManaging = () => {
-		setManaging(!managing);
-	};
+	toggleManaging() {
+		this.setState((prevState, props) => ({
+			managing: !prevState.managing
+		}));
+	}
 
-	const sendNewChat = () => {
-		if (!chatRef.current.value) return;
+	sendNewChat() {
+		if (!this.chatRef.current.value) return;
 		axios
 			.post(`${serverURL}/api/addChat/`, {
-				sid: props.sid,
-				pageNum: props.pageNum,
-				qid: props.qid,
-				body: chatRef.current.value,
+				sid: this.props.sid,
+				pageNum: this.props.pageNum,
+				qid: this.props.qid,
+				body: this.chatRef.current.value,
 				user: getDisplayName(),
 			})
 			.then((res) => {
-				chatRef.current.value = '';
+				this.chatRef.current.value = '';
 			})
-			.then(fetchQuestionDetails)
+			.then(this.fetchQuestionDetails())
 			.catch((err) => {
 				console.error(err);
 			});
-	};
+	}
 
-	const endorseChat = (cid) => {
+	endorseChat(cid) {
 		axios
 			.post(`${serverURL}/api/endorse/`, {
-				sid: props.sid,
-				pageNum: props.pageNum,
-				qid: props.qid,
+				sid: this.props.sid,
+				pageNum: this.props.pageNum,
+				qid: this.props.qid,
 				cid: cid,
 			})
-			.then(fetchQuestionDetails)
+			.then(this.fetchQuestionDetails())
 			.catch((err) => {
 				console.error(err);
 			});
-	};
+	}
 
-	// like
-	const likeChat = (cid) => {
+	likeChat(cid) {
 		axios
 			.post(`${serverURL}/api/like/`, {
-				sid: props.sid,
-				pageNum: props.pageNum,
-				qid: props.qid,
+				sid: this.props.sid,
+				pageNum: this.props.pageNum,
+				qid: this.props.qid,
 				cid: cid,
 				user: getDisplayName(),
 			})
-			.then(fetchQuestionDetails)
+			.then(this.fetchQuestionDetails())
 			.catch((err) => {
 				console.error(err);
 			});
-	};
+	}
 
-	const deleteChat = (e, cid) => {
+	deleteChat(e, cid) {
 		e.stopPropagation();
 		if (!window.confirm(`Are you sure to delete this chat?`)) return;
 
 		axios
-			.delete(`${serverURL}/api/chat?sid=${props.sid}&qid=${props.qid}&pageNum=${props.pageNum}&cid=${cid}`)
-			.then(fetchQuestionDetails)
+			.delete(`${serverURL}/api/chat?sid=${this.props.sid}&qid=${this.props.qid}&pageNum=${this.props.pageNum}&cid=${cid}`)
+			.then(this.fetchQuestionDetails())
 			.catch((err) => {
 				console.error(err);
 			});
-	};
+	}
 
-	const showOrHideToast = () => {
-		setShowToast(!showToast);
-	};
+	showOrHideToast() {
+		this.setState((prevState, props) => ({
+			showToast: !prevState.showToast
+		}));
+	}
 
-	const copyLink = async () => {
+	async copyLink() {
 		if (!navigator.clipboard) return alert('Your browser does not support accessing clipboard!');
 		await navigator.clipboard.writeText(
-			`[@${props.filename}/Page ${props.pageNum}/Q${props.qid}](${window.location.href})`
+			`[@${this.props.filename}/Page ${this.props.pageNum}/Q${this.props.qid}](${window.location.href})`
 		);
-		showOrHideToast();
-	};
+		this.showOrHideToast();
+	}
 
-	return (
-		<>
-			<ChatAreaTitle
-				title={messages.title}
-				showManage={props.isInstructor && props.isInstructorView}
-				managing={managing}
-				toggleManaging={toggleManaging}
-				showBackBtn={true}
-				back={props.back}
-			/>
-			<div className='chat-details'>
-				<div className='title' key={-2}>
-					{messages.title}
-				</div>
-				<Snackbar
-					className='toast'
-					anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-					open={showToast}
-					onClose={showOrHideToast}
-					autoHideDuration={2500}
-					message='Link copied to clipboard!'
+	render() {
+		const { managing, messages, showToast } = this.state;
+		return (
+			<>
+				<ChatAreaTitle
+					title={messages.title}
+					showManage={this.props.isInstructor && this.props.isInstructorView}
+					managing={managing}
+					toggleManaging={this.toggleManaging}
+					showBackBtn={true}
+					back={this.props.back}
 				/>
-				{messages.chats.map((message, i) => {
-					if (!message) return null;
-					return (
-						<div className='chat' key={i}>
-							<div className='info'>
-								<div>
-									<span className='author'>{`${message.user}${
-										props.isInstructor && message.uid && props.isInstructorView
+				<div className='chat-details'>
+					<div className='title' key={-2}>
+						{messages.title}
+					</div>
+					<Snackbar
+						className='toast'
+						anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+						open={showToast}
+						onClose={this.showOrHideToast}
+						autoHideDuration={2500}
+						message='Link copied to clipboard!'
+					/>
+					{messages.chats.map((message, i) => {
+						if (!message) return null;
+						return (
+							<div className='chat' key={i}>
+								<div className='info'>
+									<div>
+										<span className='author'>{`${message.user}${this.props.isInstructor && message.uid && this.props.isInstructorView
 											? ` (${message.uid})`
 											: ''
-									}`}</span>
-									<span className='time'>
-										{(message.modified ? 'Modified ' : '') + formatTime(message.time)}
-									</span>
-								</div>
-								<div className='icons'>
-									{i === 0 ? (
-										<span
-											className='material-icons icon quote'
-											title='quote this question'
-											onClick={copyLink}>
-											link
+											}`}</span>
+										<span className='time'>
+											{(message.modified ? 'Modified ' : '') + formatTime(message.time)}
 										</span>
-									) : null}
-									{message.endorsement.length ? (
-										<span className='material-icons endorsed icon' onClick={(e) => endorseChat(i)}>
-											verified
-										</span>
-									) : props.isInstructor ? (
-										<span
-											className='material-icons not-endorsed icon'
-											onClick={(e) => endorseChat(i)}>
-											verified
-										</span>
-									) : null}
-									<span className={`icon ${message.likes.length ? 'liked' : 'nobody-liked'}`}>
-										<span>{message.likes.length ? message.likes.length : ''}</span>
-										<span className='material-icons' onClick={(e) => likeChat(i)}>
-											favorite
-										</span>
-									</span>
-									{managing && i > 0 ? (
-										<span className='material-icons delete icon' onClick={(e) => deleteChat(e, i)}>
-											delete_forever
-										</span>
-									) : null}
-								</div>
-							</div>
-							<div className='body' dangerouslySetInnerHTML={{ __html: md.render(message.body) }}></div>
-							<div className='info-bottom'>
-								{message.uid === props.uid ? (
-									<div className='modify-btn' onClick={(e) => props.goToModify(message, i)}>
-										Modify
 									</div>
-								) : null}
-								{message.endorsement && message.endorsement.length > 0 ? (
-									<div className='endorsement'>{`Verified by ${formatNames(
-										message.endorsement
-									)}`}</div>
-								) : null}
+									<div className='icons'>
+										{i === 0 ? (
+											<span
+												className='material-icons icon quote'
+												title='quote this question'
+												onClick={this.copyLink}>
+												link
+											</span>
+										) : null}
+										{message.endorsement.length ? (
+											<span className='material-icons endorsed icon' onClick={(e) => this.endorseChat(i)}>
+												verified
+											</span>
+										) : this.props.isInstructor ? (
+											<span
+												className='material-icons not-endorsed icon'
+												onClick={(e) => this.endorseChat(i)}>
+												verified
+											</span>
+										) : null}
+										<span className={`icon ${message.likes.length ? 'liked' : 'nobody-liked'}`}>
+											<span>{message.likes.length ? message.likes.length : ''}</span>
+											<span className='material-icons' onClick={(e) => this.likeChat(i)}>
+												favorite
+											</span>
+										</span>
+										{managing && i > 0 ? (
+											<span className='material-icons delete icon' onClick={(e) => this.deleteChat(e, i)}>
+												delete_forever
+											</span>
+										) : null}
+									</div>
+								</div>
+								<div className='body' dangerouslySetInnerHTML={{ __html: md.render(message.body) }}></div>
+								<div className='info-bottom'>
+									{message.uid === this.props.uid ? (
+										<div className='modify-btn' onClick={(e) => this.props.goToModify(message, i)}>
+											Modify
+										</div>
+									) : null}
+									{message.endorsement && message.endorsement.length > 0 ? (
+										<div className='endorsement'>{`Verified by ${formatNames(
+											message.endorsement
+										)}`}</div>
+									) : null}
+								</div>
 							</div>
-						</div>
-					);
-				})}
-				<div className='send-message-bar' key={-1}>
-					<TextField variant='outlined' id={`chat-response`} multiline rowsMax='4' inputRef={chatRef} />
-					<Button variant='contained' color='primary' onClick={sendNewChat}>
-						Send
-					</Button>
+						);
+					})}
+					<div className='send-message-bar' key={-1}>
+						<TextField variant='outlined' id={`chat-response`} multiline rowsMax='4' inputRef={this.chatRef} />
+						<Button variant='contained' color='primary' onClick={this.sendNewChat}>
+							Send
+						</Button>
+					</div>
+					<div className='anonymity'>{anonymityMessage(this.props.anonymity, this.props.username)}</div>
 				</div>
-				<div className='anonymity'>{anonymityMessage(props.anonymity, props.username)}</div>
-			</div>
-		</>
-	);
+			</>
+		);
+	}
 }
